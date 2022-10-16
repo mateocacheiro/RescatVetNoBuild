f1ad12bc7b571625f1480ed65dbe7e830126c69a
mateocacheiro <mateocacheiro@protonmail.com>
2022-10-16 16:40:02 +0200

    Scroll logic almost done

--- components/AccordionItem.js
+++ components/AccordionItem.js
@@ -21,15 +21,13 @@ const AccordionItem = (props) => {
     if (type == "emergency") {
         title = EmergencyInfo[info_id-1]['TitleES']
         description = EmergencyInfo[info_id-1]['DescriptionES']
+    } else if (type == "care") {
+        // Get title and description from the BasicCareInfo table
+        console.log("Bruh")
     }
 
 
     const renderContent = () => {
-
-        /*
-        If there are no <sub>, render the entire content
-        If there are <sub> tags, slice the data buy the tags
-        */
 
         let splitData = []
         let dataObj = []
@@ -37,25 +35,32 @@ const AccordionItem = (props) => {
         if (description.includes("<sub>")) {
             splitData = description.split('<sub>')
             splitData.shift()
+        } else {
+            splitData = splitData.concat(description)
         }
 
-        const getStr = (splitDescription) => {
-            console.log(splitDescription)
-            let link_value = []
+        const getStr = (splitDescription, type) => {
+            let out_value = []
             {splitDescription.map(function(element){
                 if(!element.includes("<src>")) {
-                    link_value.push(<Text style={styles.text}>{element}</Text>)
-                    console.log("Link value not containing src: ", link_value)
-                } else if (element.includes("<src>")) {
+                    out_value.push(<Text style={styles.text}>{element}</Text>)
+                } else if (element.includes("<src>") && type == "link") {
                     const splitLink = element.split("<src>")
                     splitLink.shift()
                     const src_link = splitLink[0]
-                    link_value.push(<View><TouchableOpacity onPress={() => {navigation.jumpTo(src_link)}}><Text style={[styles.textBold, {marginHorizontal: 5}]}>{splitLink[1]}</Text></TouchableOpacity></View>)
+                    const text_link = splitLink[1]
+                    out_value.push(<View><TouchableOpacity onPress={() => {navigation.jumpTo(src_link)}}><Text style={[styles.textBold, {marginHorizontal: 5}]}>{text_link}</Text></TouchableOpacity></View>)
+                } else if (element.includes("<src>") && type == "scroll") {
+                    const splitScroll = element.split("<src>")
+                    splitScroll.shift()
+                    const src_scroll = splitScroll[0]
+                    const text_scroll = splitScroll[1]
+                    out_value.push(<View><TouchableOpacity onPress={() => {console.log("Scroll to"+src_scroll)}}><Text style={styles.textBold}>{text_scroll}</Text></TouchableOpacity></View>)
                 }
             })}
             return(
                 <Text>
-                    {link_value.map(function(element){
+                    {out_value.map(function(element){
                         return element
                     })}
                 </Text>
@@ -66,19 +71,25 @@ const AccordionItem = (props) => {
             <View style={styles.content}>
                 {splitData.map(function(element, idx) {
                     const e_index = splitData.indexOf(element)
-                    if (!element.includes('<link>')) {
+                    if (!element.includes('<link>') && !element.includes('<scroll>')) {
                         return(
                             <View key={idx}>
                                 <Text style={e_index%2 == 0 ? styles.title2 : styles.text}>{element}</Text>
                                 {e_index%2 == 0 && <View style={styles.subdivider} />}
                             </View>
                         )
-                    } else if (element.includes('<link>')) {
+                    } else if (element.includes('<link>') && !element.includes('<scroll>')) {
                         const splitDescription = element.split('<link>')
-                        const jsx_return = getStr(splitDescription)
                         return(
-                            getStr(splitDescription)
+                            getStr(splitDescription, type="link")
                         )
+                    } else if (element.includes('<scroll>') && !element.includes('<link>')) {
+                        const splitDescription = element.split('<scroll')
+                        return(
+                            getStr(splitDescription, type="scroll")
+                        )
+                    } else if (element.includes('<link>') && element.includes('<scroll>')) {
+                        // Brainfuck level 1000
                     }
                 })}
             </View>
